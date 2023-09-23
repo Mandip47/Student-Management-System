c7cb46575606601b35655dca13fcfb24e9734a60
Mandip47 <108076760+Mandip47@users.noreply.github.com>
2023-09-23 09:50:53 +0000

    System clear added

--- .ccls-cache/@home@runner@HS-other-functionalities-like-storing-attendance/main.c
+++ .ccls-cache/@home@runner@HS-other-functionalities-like-storing-attendance/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-//structure
+// structure
 struct Student {
   char name[50];
   int age;
@@ -12,17 +13,18 @@ struct Student {
 
 // functions declaration
 void calculatePERC(struct Student *student);
-float percentage(float total,float fM);
+float percentage(float total, float fM);
 void inputAttendance(struct Student *student);
 
-//main function 
+// main function
 int main() {
   struct Student students[100];
   FILE *fptr;
-  int choice, num_students = 0,num_saved_stds=1;
+  int choice, num_students = 0, num_saved_stds = 1;
 
-  do {   
-printf("------------------------------------------------"); printf("\n*********************Menu***********************\n");
+  do {
+    printf("------------------------------------------------");
+    printf("\n*********************Menu***********************\n");
     printf("------------------------------------------------\n");
     printf("1. Add Student\n");
     printf("2. Calculate Percentage\n");
@@ -35,11 +37,12 @@ printf("------------------------------------------------"); printf("\n**********
     printf("Enter your choice: ");
     scanf("%d", &choice);
     getchar();
+    system("clear");
     switch (choice) {
     case 1:
       printf("\n------------------------------------------------");
-printf("\n****************Student Form********************\n");
-    printf("------------------------------------------------\n");
+      printf("\n****************Student Form********************\n");
+      printf("------------------------------------------------\n");
       printf("Enter name: ");
       fgets(students[num_students].name, 50, stdin);
       printf("Enter age: ");
@@ -48,27 +51,32 @@ printf("\n****************Student Form********************\n");
       printf("Enter major: ");
       fgets(students[num_students].major, 50, stdin);
       // getchar();
-      students[num_students].percentage = 0; 
+      students[num_students].percentage = 0;
       students[num_students].attendance = 0;
       num_students++;
+      system("clear");
       printf("\nStudent Added Successfully!!\n");
       printf("\n");
       break;
-      
+
     case 2:
-      
+
       if (num_students > 0) {
         int student_index;
         printf("\n------------------------------------------------\n");
         printf("Enter student index (0 to %d): ", num_students - 1);
         scanf("%d", &student_index);
         if (student_index >= 0 && student_index < num_students) {
-calculatePERC(&students[student_index]);
-          printf("\nPercentage calculated for %s\n", students[student_index].name);
+          calculatePERC(&students[student_index]);
+          system("clear");
+          printf("\nPercentage calculated for %s\n",
+                 students[student_index].name);
         } else {
+          system("clear");
           printf("\n⚠ Invalid student index\n");
         }
       } else {
+        system("clear");
         printf("\n⚠No students to calculate Percentage\n");
       }
       printf("\n");
@@ -81,70 +89,75 @@ calculatePERC(&students[student_index]);
         scanf("%d", &student_index);
         if (student_index >= 0 && student_index < num_students) {
           inputAttendance(&students[student_index]);
-          printf("Attendance recorded for %s!!!\n", students[student_index].name);
+          system("clear");
+          printf("Attendance recorded for %s!!!\n",
+                 students[student_index].name);
         } else {
+          system("clear");
           printf("\n⚠Invalid student index\n");
         }
       } else {
+        system("clear");
         printf("\n⚠No students to record attendance\n");
       }
       printf("\n");
       break;
-      
+
     case 4:
       if (num_students > 0) {
         printf("\nRecently added Students:\n");
         for (int i = 0; i < num_students; i++) {
-printf("-----------------------\n");
-printf("Name: %s", students[i].name);
-printf("Age: %d\n", students[i].age);
-printf("Major: %s", students[i].major);
-printf("Percentage: %.2f%\n", students[i]. percentage);
-printf("Attendance: %d/30\n", students[i].attendance);
-printf("-----------------------\n");
-printf("You should save it permanently!!\n");
-      }
+          printf("-----------------------\n");
+          printf("Name: %s", students[i].name);
+          printf("Age: %d\n", students[i].age);
+          printf("Major: %s", students[i].major);
+          printf("Percentage: %.2f%\n", students[i].percentage);
+          printf("Attendance: %d/30\n", students[i].attendance);
+          printf("-----------------------\n");
+          printf("You should save it permanently!!\n");
+        }
       } else {
         printf("\n⚠No students to display\n");
       }
       break;
-      
+
     case 5:
       if (num_saved_stds > 0) {
-        fptr = fopen("stdsData.txt","r");
+        fptr = fopen("stdsData.txt", "r");
         char ch;
         ch = fgetc(fptr);
-        
-        while(ch != EOF){
-          printf("%c",ch);
-          ch=fgetc(fptr);
+
+        while (ch != EOF) {
+          printf("%c", ch);
+          ch = fgetc(fptr);
         }
         fclose(fptr);
       } else {
         printf("\n⚠No students to display\n");
-            }
-      break;
-      
-    case 6:
-      if (num_students > 0){
-     fptr = fopen("stdsData.txt","a");
-      for (int i = 0; i < num_students; i++) {
-    fprintf(fptr,"-----------------------\n");
-fprintf(fptr,"Name: %s", students[i].name);
-fprintf(fptr,"Age: %d\n", students[i].age);
-fprintf(fptr,"Major: %s", students[i].major);
-fprintf(fptr,"Percentage: %.2f%\n", students[i]. percentage);
-fprintf(fptr,"Attendance: %d/30\n", students[i].attendance);
-fprintf(fptr,"-----------------------\n");
-num_saved_stds++;
       }
-    fclose(fptr);
-    printf("\nData Successfully saved \n");
-      }else {
+      break;
+
+    case 6:
+      if (num_students > 0) {
+        fptr = fopen("stdsData.txt", "a");
+        for (int i = 0; i < num_students; i++) {
+          fprintf(fptr, "-----------------------\n");
+          fprintf(fptr, "Name: %s", students[i].name);
+          fprintf(fptr, "Age: %d\n", students[i].age);
+          fprintf(fptr, "Major: %s", students[i].major);
+          fprintf(fptr, "Percentage: %.2f%\n", students[i].percentage);
+          fprintf(fptr, "Attendance: %d/30\n", students[i].attendance);
+          fprintf(fptr, "-----------------------\n");
+          num_saved_stds++;
+        }
+        fclose(fptr);
+        printf("\nData Successfully saved \n");
+      } else {
         printf("\n⚠No students to save!!\n");
       }
       break;
     case 7:
+      system("clear");
       printf("\nExiting program\n");
       break;
     default:
@@ -155,42 +168,38 @@ num_saved_stds++;
   return 0;
 }
 
-//Functions defination 
+// Functions defination
 
 void calculatePERC(struct Student *student) {
 
-  float phy,che,mat,nep,eng,comp,fM, percent;
-  
+  float phy, che, mat, nep, eng, comp, fM, percent;
+
   printf("\nEnter full marks : ");
-  scanf("%f",&fM);
+  scanf("%f", &fM);
   printf("\nEnter physics marks : ");
-  scanf("%f",&phy);
+  scanf("%f", &phy);
   printf("\nEnter chemistry marks : ");
-  scanf("%f",&che);
+  scanf("%f", &che);
   printf("\nEnter math marks : ");
-  scanf("%f",&mat);
+  scanf("%f", &mat);
   printf("\nEnter nepali marks : ");
-  scanf("%f",&nep);
+  scanf("%f", &nep);
   printf("\nEnter english marks : ");
-  scanf("%f",&eng);
+  scanf("%f", &eng);
   printf("\nEnter computer marks : ");
-  scanf("%f",&comp);
-  float total = phy+che+mat+nep+eng+comp;
-  
-  percent = percentage(total,fM);
-  
+  scanf("%f", &comp);
+  float total = phy + che + mat + nep + eng + comp;
+
+  percent = percentage(total, fM);
+
   student->percentage = percent;
-  
 }
 
 void inputAttendance(struct Student *student) {
-int att;
+  int att;
   printf("\nEnter this month attendence:");
-  scanf("%d",&att);
-  student->attendance=att;
-  
+  scanf("%d", &att);
+  student->attendance = att;
 }
 
-float percentage(float total,float fM){
-  return (total/(fM * 6)) * 100;
-}
+float percentage(float total, float fM) { return (total / (fM * 6)) * 100; }
